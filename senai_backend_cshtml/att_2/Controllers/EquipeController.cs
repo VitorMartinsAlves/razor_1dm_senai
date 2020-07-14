50806e93cfee8ed209cf32d04f4fc510dbd88096
VitorMartinsAlves <vitormartinalves99@gmail.com>
2020-07-13 21:37:37 -0300

    testando o cadastro com importes locais

---
+++ senai_backend_cshtml/att_2/Controllers/EquipeController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace E_Players.Controllers {
+    public class EquipeController : Controller
+
+    {
+        Equipe equipeModel = new Equipe ();
+        public IActionResult Index () {
+            ViewBag.Equipes = equipeModel.ReadAll ();
+            return View ();
+        }
+        public IActionResult Cadastrar (IFormCollection form) {
+            Equipe novaEquipe = new Equipe ();
+            novaEquipe.IdEquipe = Int32.Parse (form["IdEquipe"]);
+            novaEquipe.Nome = form["Nome"];
+            novaEquipe.Imagem = form["Imagem"];
+
+            equipeModel.Create (novaEquipe);
+            ViewBag.Equipes = equipeModel.ReadAll ();
+
+            return LocalRedirect ("~/Equipe");
+        }
+    }
+}
